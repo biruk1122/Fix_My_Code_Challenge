97a7bdb5d6414f7c2c2d801225422207d863e5b4
biruk1122 <birukfikadu29@gmail.com>
2023-12-19 02:27:42 -0800

    js_fix

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,22 +1,29 @@
 #!/usr/bin/node
 /*
     Print a square with the character #
+    
     The size of the square must be the first argument 
     of the program.
 */
-
 
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1)
+    process.exit(1);
 }
 
-size = parseInt(process.argv[2], 10)
+// Parse the command-line argument as a decimal value
+let size = parseInt(process.argv[2], 10);
 
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
+// Check if the parsed size is a valid number
+if (isNaN(size)) {
+    process.stderr.write("Invalid size argument. Please provide a valid number.\n");
+    process.exit(1);
+}
+
+for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
